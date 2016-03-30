40d323b518eb0f7f9c5d38b873e4bc48082808a9
Gegam <gegam-92@inbox.ru>
2016-03-30 20:01:56 +0300

    Update client.js

--- client.js
+++ client.js
@@ -13,22 +13,10 @@
                 html.clientHeight, html.scrollHeight, html.offsetHeight);
         }())
     }
-    var fit = JSON.stringify(iframe);
+    var config = JSON.stringify(iframe);
     window.iframeParent = {
         init: function (url) {
-            parent.postMessage(fit, url);
-            if (window.addEventListener) {
-                window.addEventListener('message', this.getMessage.bind(this));
-            } else {
-                window.attachEvent('onmessage', this.getMessage.bind(this));
-            }
-        },
-        getMessage: function (event) {
-            if (event.origin === location.protocol + '//' + location.host) {
-                var subscribe = event.data;
-                subscribe = JSON.parse(subscribe);
-                console.log(subscribe);
-            }
+            parent.postMessage(config, url);
         }
     }
 }());
